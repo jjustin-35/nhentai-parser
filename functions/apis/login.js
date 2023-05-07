0fe3753ae544598bde13cff87f3b41e9102dc906
jjustin-chen <justin.chen@kdanmobile.com>
2023-05-07 12:21:26 +0800

    update: file path

--- functions/apis/login.js
+++ functions/apis/login.js
@@ -1,5 +1,5 @@
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import app from '../functions/config/firebase.js';
+import app from '../config/firebase.js';
 
 const auth = getAuth(app);
 
